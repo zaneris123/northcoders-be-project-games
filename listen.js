6a935ce7c41568391a21b74f3f26e9d3c59227d8
zaneris123 <zaneris123@gmail.com>
2023-03-30 10:24:21 +0100

    Setting up repo for host

--- listen.js
+++ listen.js
@@ -1,6 +1,7 @@
 const app = require("./app.js")
+const { PORT = 9090 } = process.env
 
-app.listen(9090, (err) => {
+app.listen(PORT, (err) => {
     if(err) console.log(err);
-    else console.log("Listening on port 9090.")
+    else console.log(`Listening on port ${PORT}...`)
 });
